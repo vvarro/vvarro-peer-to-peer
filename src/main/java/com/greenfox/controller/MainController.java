a26cbdbbf71fc620f8a46fdb94de535a0f5cd6b5
VVarro <virag.varro@gmail.com>
2017-05-17 16:32:25 +0200

    new function: create log

--- src/main/java/com/greenfox/controller/MainController.java
+++ src/main/java/com/greenfox/controller/MainController.java
@@ -1,13 +1,23 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.Log;
+import java.text.ParseException;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-  public class MainController {
+public class MainController {
 
-    @RequestMapping()
-    public String welcome(){
-      return "index";
+  @RequestMapping(value = "/")
+  public String log(HttpServletRequest request) throws ParseException {
+    if(System.getenv("CHAT_APP_LOGLEVEL").equals("ERROR")) {
+    System.err.println("ERROR");
+    } else {
+      Log log = new Log(request);
+      System.out.println(log);
     }
+    return "index";
   }
+
+}
---
+++ src/main/java/com/greenfox/model/Log.java
@@ -0,0 +1,65 @@
+package com.greenfox.model;
+
+import java.text.ParseException;
+import javax.servlet.http.HttpServletRequest;
+
+public class Log {
+
+  String path;
+  String method;
+  long dateandtime;
+  String request;
+  String loglevel;
+  String requestdata;
+  String message;
+
+  public Log(HttpServletRequest request) throws ParseException {
+    this.dateandtime = request.getSession().getCreationTime();
+    this.loglevel = System.getProperty("CHAT_APP_LOGLEVEL");
+    this.request = "Request";
+    this.path = request.getServletPath();
+    this.method = request.getMethod();
+    this.message = request.getPathInfo();
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public String getMethod() {
+    return method;
+  }
+
+  public void setMethod(String method) {
+    this.method = method;
+  }
+
+  public long getDateandtime() {
+    return dateandtime;
+  }
+
+  public void setDateandtime(long dateandtime) {
+    this.dateandtime = dateandtime;
+  }
+
+  public String getLoglevel() {
+    return loglevel;
+  }
+
+  public void setLoglevel(String loglevel) {
+    this.loglevel = loglevel;
+  }
+
+  public String getRequestdata() {
+    return requestdata;
+  }
+
+  public void setRequestdata(String requestdata) {
+    this.requestdata = requestdata;
+  }
+
+}
