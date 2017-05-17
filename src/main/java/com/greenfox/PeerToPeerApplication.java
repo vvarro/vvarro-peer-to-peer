199cb6e619b6f72818d5db8d03f452461ed4f51c
VVarro <virag.varro@gmail.com>
2017-05-17 14:00:41 +0200

    another new project

---
+++ src/main/java/com/greenfox/PeerToPeerApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerToPeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerToPeerApplication.class, args);
+	}
+}
