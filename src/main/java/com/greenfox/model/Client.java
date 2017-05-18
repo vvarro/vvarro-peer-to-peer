47f5a2d5686bcdf01d215b352dfd479de595dbf0
VVarro <virag.varro@gmail.com>
2017-05-18 16:52:48 +0200

    create new classes

---
+++ src/main/java/com/greenfox/model/Client.java
@@ -0,0 +1,20 @@
+package com.greenfox.model;
+
+public class Client {
+  String id;
+
+  public Client(String id) {
+    this.id = id;
+  }
+
+  public Client() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+}
--- src/main/java/com/greenfox/model/Message.java
+++ src/main/java/com/greenfox/model/Message.java
@@ -1,5 +1,54 @@
 package com.greenfox.model;
 
-public class Message {
+import java.sql.Timestamp;
 
+public class Message {
+  long id;
+  String username;
+  String text;
+  Timestamp timestamp;
+
+  public Message() {
+    this.timestamp = new Timestamp(System.currentTimeMillis());;
+  }
+
+  public Message(long id, String username, String text, Timestamp timestamp) {
+    this.id = id;
+    this.username = username;
+    this.text = text;
+    this.timestamp = timestamp;
+  }
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public Timestamp getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Timestamp timestamp) {
+    this.timestamp = timestamp;
+  }
 }
