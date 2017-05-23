598444a5fcd6fdc8a6d58b2f381df736d989a38e
VVarro <virag.varro@gmail.com>
2017-05-23 10:18:33 +0200

    refactor

--- src/main/java/com/greenfox/controller/MainController.java
+++ src/main/java/com/greenfox/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.greenfox.controller;
 
-import com.greenfox.model.Log;
 import com.greenfox.model.Message;
 import com.greenfox.repository.MessageRepository;
 import com.greenfox.repository.UserRepository;
@@ -18,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
+
   private static final String url = System.getenv("CHAT_APP_PEER_ADDRESS") + "/api/message/receive";
   private static final String privateId = System.getenv("CHAT_APP_UNIQUE_ID");
 
@@ -33,26 +33,26 @@ public class MainController {
   @GetMapping("/")
   public String mainPage(HttpServletRequest request, Model model) throws Exception {
     mainControllerService.callEndpoint(request);
-      model.addAttribute("error", errorNoUser);
-      model.addAttribute("username", userRepository.findOne((long) 1).getName());
-      model.addAttribute("messages", messageRepository.findAllByOrderByTimestampAsc());
-      return "index";
-    }
+    model.addAttribute("error", errorNoUser);
+    mainControllerService.addAttribute(model);
+//    model.addAttribute("username", userRepository.findOne((long) 1).getName());
+//    model.addAttribute("messages", messageRepository.findAll());
+    return "index";
+  }
 
   @GetMapping("/update")
   public String updateUser(@RequestParam("newusername") String newUsername,
       HttpServletRequest request, Model model) throws Exception {
-    Log log = new Log(request);
-    if (userRepository.count() == 0) {
-      return "redirect:/enter";
-    } else if (newUsername.isEmpty()) {
+    mainControllerService.callEndpoint(request);
+    if (newUsername.isEmpty()) {
       errorNoUser = "The username field is empty";
       return "redirect:/";
     } else {
       userRepository.findOne((long) 1).setName(newUsername);
       userRepository.save(userRepository.findOne((long) 1));
-      model.addAttribute("username", userRepository.findOne((long) 1).getName());
-      model.addAttribute("messages", messageRepository.findAllByOrderByTimestampAsc());
+      mainControllerService.addAttribute(model);
+//    model.addAttribute("username", userRepository.findOne((long) 1).getName());
+//    model.addAttribute("messages", messageRepository.findAll());
       return "index";
     }
   }
@@ -60,21 +60,16 @@ public class MainController {
   @PostMapping("/send")
   public String sendMessage(@RequestParam("send") String send,
       HttpServletRequest request, Model model) throws Exception {
-    Log log = new Log(request);
-    if (userRepository.count() == 0) {
-      return "redirect:/enter";
-    } else {
-      ClientMessage clientMessage = new ClientMessage();
-      clientMessage.getClient().setId(userRepository.findOne((long) 1).getName());
-      clientMessage.setMessage(new Message(send, userRepository.findOne((long) 1).getName()));
-      RestTemplate restTemplate = new RestTemplate();
-      restTemplate.postForObject(url, clientMessage,ResponseMessage.class);
-      messageRepository.save(new Message(send, userRepository.findOne((long) 1).getName()));
-      model.addAttribute("username", userRepository.findOne((long) 1).getName());
-      model.addAttribute("messages", messageRepository.findAll());
-      return "redirect:/";
-    }
+    mainControllerService.callEndpoint(request);
+    ClientMessage clientMessage = new ClientMessage();
+    clientMessage.getClient().setId(userRepository.findOne((long) 1).getName());
+    clientMessage.setMessage(new Message(send, userRepository.findOne((long) 1).getName()));
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.postForObject(url, clientMessage, ResponseMessage.class);
+    messageRepository.save(new Message(send, userRepository.findOne((long) 1).getName()));
+    mainControllerService.addAttribute(model);
+//    model.addAttribute("username", userRepository.findOne((long) 1).getName());
+//    model.addAttribute("messages", messageRepository.findAll());
+    return "redirect:/";
   }
-
-
 }
--- src/main/java/com/greenfox/service/MainControllerService.java
+++ src/main/java/com/greenfox/service/MainControllerService.java
@@ -1,17 +1,21 @@
 package com.greenfox.service;
 
 import com.greenfox.model.Log;
+import com.greenfox.repository.MessageRepository;
 import com.greenfox.repository.UserRepository;
 import java.text.ParseException;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 
 @Component
 public class MainControllerService {
 
   @Autowired
   UserRepository userRepository;
+  @Autowired
+  MessageRepository messageRepository;
 
   public String callEndpoint(HttpServletRequest request) throws ParseException {
     Log log = new Log(request);
@@ -19,5 +23,10 @@ public class MainControllerService {
       return "redirect:/enter";
     }
     return "";
+  }
+
+  public void addAttribute(Model model){
+    model.addAttribute("username", userRepository.findOne((long) 1).getName());
+    model.addAttribute("messages", messageRepository.findAll());
   }
 }
