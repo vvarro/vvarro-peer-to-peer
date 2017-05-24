9ae930a2239efd61a6c7a92008620d2b7428df66
VVarro <virag.varro@gmail.com>
2017-05-24 14:30:23 +0200

    add refresh meta

---
+++ src/main/java/com/greenfox/model/GoTo.java
@@ -0,0 +1,16 @@
+package com.greenfox.model;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GoTo extends HttpServlet {
+
+  public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,
+      IOException {
+    String url = "https://vvarro-p2p.herokuapp.com";
+    res.sendRedirect(url);
+  }
+}
